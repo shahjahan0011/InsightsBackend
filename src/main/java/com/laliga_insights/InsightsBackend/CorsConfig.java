c1e80bfa88fcc7318451d61e93e805a894aa3bf9
Jahan Shah <85148797+shahjahan0011@users.noreply.github.com>
2025-07-23 00:42:30 -0700

    .

--- src/main/java/com/laliga_insights/InsightsBackend/CorsConfig.java
+++ src/main/java/com/laliga_insights/InsightsBackend/CorsConfig.java
@@ -2,6 +2,7 @@ package com.laliga_insights.InsightsBackend;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,7 +12,7 @@ public class CorsConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings (CorsRegistry registry){
+            public void addCorsMappings (@NonNull CorsRegistry registry){
                 registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:3000", "https://laliga-insights.vercel.app/")
                         .allowedMethods("GET")
